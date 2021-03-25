3dd6d489c544aba61ed14e5351060fcabecb84c8
NOhhwasung <sinical909@naver.com>
2021-03-25 14:22:40 +0900

    TODO

--- App.js
+++ App.js
@@ -2,27 +2,52 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 import { StyleSheet, Text, View, ScrollView, SafeAreaView, Image, TextInput, Dimensions, Platform, TouchableOpacity, Alert } from 'react-native';
-import TodoInsert from './components/TodoInsert'
-import TodoList from './components/TodoList'
-import TodoListItem from './components/TodoListItem'
 import data from './data.json'
-import start from './assets/start.png';
-import Icon from 'react-native-vector-icons/AntDesign';
-import CheckBox from '@react-native-community/checkbox';
-
+import stay from './assets/stay.png';
+import Card from './components/Card'
+import Loading from './components/Loading'
+import AppLoading from 'expo-app-loading'
+import {
+  useFonts,
+  Inter_900Black,
+} from '@expo-google-fonts/inter';
 
 
 
 
 const { height, width } = Dimensions.get('window');
+// const fetchFonts = async () => {
+//   await Font.loadAsync({
+//     'BMJUA': require('./assets/fonts/BMJUA.ttf'),
+//   });
+// }
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    Inter_900Black,
+  });
 
+
+
+  // const [fontLoaded, setFontLoaded] = useState(true);
+  // if (!fontLoaded) {
+  //   return (
+  //     <AppLoading
+  //       startAsync={fetchFonts}
+  //       onFinish={setFontLoaded(true)} 
+  //       onError={alert('오류')}/>
+  //   );
+  // }
 
   console.disableYellowBox = true;
 
   const [todos, setTodos] = useState([]);
-  const [toggleCheckBox, setToggleCheckBox] = useState(false)
+  // const [toggleCheckBox, setToggleCheckBox] = useState(false)
+  // const [cateState,setCateState] = useState([])
+  // const [checked, setChecked] = React.useState(false);
+
+  const [ready, setReady] = useState(true)
+
 
   const popup = () => {
     Alert.alert('오늘도 파이팅!')
@@ -31,46 +56,47 @@ export default function App() {
   //   Alert.alert('팝업 알람입니다!!')
   // }}
 
-
   useEffect(() => {
-    setTodos(data)
+
+    //뒤의 1000 숫자는 1초를 뜻함
+    //1초 뒤에 실행되는 코드들이 담겨 있는 함수
+    setTimeout(() => {
+      setTodos(data)
+      setReady(false)
+    }, 1500)
+
+
   }, [])
+  let todo = todos.tip;
 
-  let todo = data.tip;
-  let index = data.tip.idx;
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barstyle="auto" />
-      <Text style={styles.apptitle}>TODAY'S MAIN</Text>
-      <TouchableOpacity onPress={() => popup()}>
-        <Image
-          source={start}
-          resizeMode={'contain'}
-          style={styles.image}
-        />
-      </TouchableOpacity>
-      < ScrollView style={styles.card}>
-        {
-          todo.map((content, i) => {
-            return (<View style={styles.todocontainer} key={i}>
-              <CheckBox
-                disabled={false}
-                value={toggleCheckBox}
-                tintColors={{ true: 'blue' }}
-                onValueChange={() => setToggleCheckBox(!toggleCheckBox)}
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return ready ? <Loading /> : (
+      <SafeAreaView style={styles.container}>
+        <StatusBar barstyle="auto" />
+        <Text style={styles.apptitle}>TODAY'S MAIN</Text>
+        <TouchableOpacity onPress={() => popup()}>
+          <Image
+            source={stay}
+            resizeMode={'contain'}
+            style={styles.image}
+          />
+        </TouchableOpacity>
+        < ScrollView style={styles.card}>
+          {
+            todo.map((content, i) => {
+              return (
+                <Card content={content} key={i} />
+              )
+            })
+          }
+        </ScrollView>
+      </SafeAreaView>
+    )
 
-              />
-              <Text style={styles.text, toggleCheckBox ? styles.strikeText : styles.unstrikeText}>{content.title} </Text>
-            </View>)
-          })
-        }
-      </ScrollView>
-    </SafeAreaView>
-  )
-
+  }
 }
-
-// 
 
 
 const styles = StyleSheet.create({
@@ -84,9 +110,11 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 29,
     fontWeight: '300',
+    fontFamily: 'Inter_900Black',
     textAlign: 'center',
     marginTop: 35,
-    marginBottom: 10
+    marginBottom: 10,
+    marginLeft: 10
   },
   card: {
     backgroundColor: 'white',
@@ -136,6 +164,7 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     fontSize: 18,
     textAlign: 'center',
+    fontFamily:'Inter_900Black',
     marginVertical: 20,
     width: 100,
     marginLeft: 20
@@ -171,4 +200,3 @@ const styles = StyleSheet.create({
     alignSelf: "center",
   },
 });
-{/* <Icon style={styles.icon} name="checkcircleo" size={30} color="pink" /> */ }
--- components/Card.js
+++ components/Card.js
@@ -1,25 +1,86 @@
 import React, { useState } from 'react'
-import CheckBox from 'react-native-check-box'
+// import CheckBox from 'react-native-check-box'
 import { Checkbox } from 'react-native-paper';
 import { StyleSheet, View, Text } from 'react-native';
+import AppLoading from 'expo-app-loading'
+import { useFonts, Allan_400Regular, Allan_700Bold } from '@expo-google-fonts/allan';
+import Icon from 'react-native-vector-icons/AntDesign';
 
 
-export default function Item({ content }) {
 
-    const [checked, setChecked] = useState(false);
-  
+
+export default function Card({ content }) {
+
+  const [checked, setChecked] = useState(false);
+  let [fontsLoaded] = useFonts({
+    Allan_400Regular,
+    Allan_700Bold,
+  });
+  let fontSize = 20;
+  let paddingVertical = 6;
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
     return (
       <View
-        style={styles.item}>
+        style={styles.todocontainer}>
         <Checkbox
           status={checked ? 'checked' : 'unchecked'}
           onPress={() => {
             setChecked(!checked);
           }}
-          color="#262C74"
+          color="blue"
         />
-        <Text 
-          style={[checked ? styles.strikeText : styles.unstrikeText]}>{task.title}</Text>
+        <Text style={{
+          fontSize,
+          paddingVertical, fontFamily: 'Allan_700Bold'
+        }, checked ? styles.strikeText : styles.unstrikeText}>{content.title}</Text>
+        <Icon name="rocket1" size={30} color="#000" />
       </View>
     )
   }
+}
+
+
+
+const styles = StyleSheet.create({
+
+  text: {
+    flex: 5,
+    fontWeight: '500',
+    fontSize: 18,
+    textAlign: 'center',
+    fontFamily: 'Inter_900Black',
+    marginVertical: 20,
+    width: 100,
+    marginLeft: 20
+  },
+  todocontainer: {
+    borderBottomColor: '#bbb',
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: 15,
+  },
+
+  strikeText: {
+    flex: 1,
+    marginTop: 20,
+    marginBottom: 10,
+    fontSize: 20,
+    color: '#bbb',
+    textDecorationLine: 'line-through',
+    textAlign: 'center'
+  },
+  unstrikeText: {
+    flex: 1,
+    marginTop: 20,
+    marginBottom: 10,
+    fontSize: 20,
+    textAlign: 'center',
+    color: '#29323c',
+  },
+
+});
